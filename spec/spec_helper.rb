a6f64e319cf9c95b772cf021feb3c39c62f8b5af
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-09-17 21:03:32 -0400

    directly add token to spec_Helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,8 @@
+# Something weird is going on with the documented setup...
+if ENV['TRAVIS']
+  ENV['CODECLIMATE_REPO_TOKEN'] = 'd1d3df95e4c3d80f789dd38e0af3efa1eb7f18485d18fee6fdd754e3e08f858b'
+end
+
 if ENV['CODECLIMATE_REPO_TOKEN']
   require "codeclimate-test-reporter"
   CodeClimate::TestReporter.start
