23545a8ddce331a3a66ab5e7ca6607b444cbe999
L. Preston Sego III <LPSego3+dev@gmail.com>
2016-10-28 08:24:09 -0400

    cleanup

--- lib/rails_module_unification/action_view/resource_resolver.rb
+++ lib/rails_module_unification/action_view/resource_resolver.rb
@@ -13,32 +13,5 @@ module RailsModuleUnification
       super(path)
       @path = path
     end
-
-    # def find_templates(name, prefix, partial, details)
-    #   binding.pry
-    #   super(name, prefix, partial, details)
-    # end
-    #
-    # def build_query(path, details)
-    #
-    #   query = @pattern.dup
-    #
-    #   binding.pry
-    #   prefix = path.prefix.empty? ? '' : "#{escape_entry(path.prefix)}\\1"
-    #   query.gsub!(/:prefix(\/)?/, prefix)
-    #
-    #   partial = escape_entry(path.partial? ? "_#{path.name}" : path.name)
-    #   query.gsub!(/:action/, partial)
-    #
-    #   details.each do |ext, candidates|
-    #     if ext == :variants && candidates == :any
-    #       query.gsub!(/:#{ext}/, "*")
-    #     else
-    #       query.gsub!(/:#{ext}/, "{#{candidates.compact.uniq.join(',')}}")
-    #     end
-    #   end
-    #   puts File.expand_path(query, @path)
-    #   File.expand_path(query, @path)
-    # end
   end
 end
