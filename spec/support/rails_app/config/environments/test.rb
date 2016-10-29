36a3856b432594f6d5ad64b03ffd4ef362ad84ac
L. Preston Sego III <LPSego3+dev@gmail.com>
2016-10-28 23:03:02 -0400

    comment out another old option

--- spec/support/rails_app/config/environments/test.rb
+++ spec/support/rails_app/config/environments/test.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.eager_load = false
 
   # Configure public file server for tests with Cache-Control for performance.
-  config.public_file_server.enabled = true
+  # config.public_file_server.enabled = true
   # config.public_file_server.headers = {
   #   'Cache-Control' => 'public, max-age=3600'
   # }
