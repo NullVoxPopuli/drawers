f5af3482f45072f764444c969435fc6885c2bc18
L. Preston Sego III <LPSego3+dev@gmail.com>
2016-10-28 22:55:23 -0400

    comment out another old option

--- spec/support/rails_app/config/environments/development.rb
+++ spec/support/rails_app/config/environments/development.rb
@@ -18,9 +18,9 @@ Rails.application.configure do
     config.action_controller.perform_caching = true
 
     config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      'Cache-Control' => 'public, max-age=172800'
-    }
+    # config.public_file_server.headers = {
+    #   'Cache-Control' => 'public, max-age=172800'
+    # }
   else
     config.action_controller.perform_caching = false
 
